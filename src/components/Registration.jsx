8ad2db60ba650d6eba612de65134bb4f8b9902b7
sivakrishna <gedelashipa.1321@gmail.com>
2024-12-24 10:27:38 +0530

    Dates updated

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -146,15 +146,15 @@ export default function Registration() {
               <ul className="space-y-4">
                 <li className="flex items-center">
                   <Clock className="w-5 h-5 mr-2" />
-                  <span>Registration Deadline: March 1, 2024</span>
+                  <span>Registration Deadline: March 1, 2025</span>
                 </li>
                 <li className="flex items-center">
                   <Clock className="w-5 h-5 mr-2" />
-                  <span>Document Verification: March 5, 2024</span>
+                  <span>Document Verification: March 5, 2025</span>
                 </li>
                 <li className="flex items-center">
                   <Clock className="w-5 h-5 mr-2" />
-                  <span>Exam Date: March 15, 2024</span>
+                  <span>Exam Date: March 15, 2025</span>
                 </li>
               </ul>
             </div>
